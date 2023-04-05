9f7499db52b3bdcb903c68c8c6fc41dc7901ae79
Abdr <102943880+Asan1900@users.noreply.github.com>
2023-04-05 14:16:57 +0600

    .
    .

---
+++ nft/hardhat.congif.js
@@ -0,0 +1,14 @@
+require("'@hardhat'");
+require("@nomicfoundation/hardhat-toolbox")
+require("dotenv")
+require('chai')
+require('axios')
+require('ipfs-http-client')
+
+/*** 
+ * @type import('hardhat/config').HardhatUserConfig 
+ ***/
+
+module.exports = {
+  solidity: "0.8.18",
+};
